ae8f20722ac7eda180816c7c58dbdd92d194c55d
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-07-28 00:17:26 +0530

    feat: add TweetEmbed component with caption support and update documentation

--- src/components/TweetEmbed.tsx
+++ src/components/TweetEmbed.tsx
@@ -3,16 +3,18 @@ import { Tweet, type TweetProps } from "react-tweet";
 
 export type TweetEmbedTypes = TweetProps & {
 	theme?: "light" | "dark";
+	caption?: string;
 };
 
 export const TweetEmbed = (props: TweetEmbedTypes) => {
 	const isDark = useDark();
 	const defaultTheme = isDark ? "dark" : "light";
-	const { theme = defaultTheme, ...restProps } = props;
+	const { theme = defaultTheme, caption, ...restProps } = props;
 
 	return (
 		<figure data-theme={theme}>
 			<Tweet {...restProps} />
+			{caption && <figcaption>{caption}</figcaption>}
 		</figure>
 	);
 };
